7007c3f5cf63b605916f877e9150134da1239137
Daniel Ranchal <danitico98@gmail.com>
2018-01-08 19:19:48 +0100

    En vez de poner que el socio más antiguo es el 2, se procede a cambiar ese 2 por la query que calcula ese id de socio, siendo la query más genérica

--- P4/ejercicio6.sql
+++ P4/ejercicio6.sql
@@ -1,10 +1,19 @@
 SELECT ID_SOCIO, SUM(PREMIO), CASE 
-    WHEN ID_SOCIO = ANY(SELECT DISTINCT ID_SOCIO FROM PARTICIPANTES WHERE ID_SOCIO !=2) THEN SUM(PREMIO)+(SELECT SUM(PREMIO) FROM PARTICIPANTES WHERE ID_SOCIO=2)/((SELECT COUNT (DISTINCT ID_SOCIO) FROM PARTICIPANTES)-1)
+    WHEN ID_SOCIO != (SELECT ID_SOCIO "SOCIO MÁS ANTIGUO"
+                      FROM SOCIO
+                      WHERE FECHA_ALTA = ANY (SELECT MIN(FECHA_ALTA)
+                                              FROM SOCIO 
+                                              WHERE FECHA_BAJA IS NULL)) 
+    THEN SUM(PREMIO)+(SELECT SUM(PREMIO) 
+                      FROM PARTICIPANTES
+                      WHERE ID_SOCIO=(SELECT ID_SOCIO "SOCIO MÁS ANTIGUO"
+                                      FROM SOCIO
+                                      WHERE FECHA_ALTA = ANY (SELECT MIN(FECHA_ALTA)
+                                                              FROM SOCIO 
+                                                              WHERE FECHA_BAJA IS NULL)))/((SELECT COUNT (DISTINCT ID_SOCIO) FROM PARTICIPANTES)-1)
     ELSE 0
     END
     "Nuevo"           
 FROM PARTICIPANTES
 GROUP BY ID_SOCIO
 ORDER BY ID_SOCIO;
-
-
--- P4/ejercicio7.sql
+++ P4/ejercicio7.sql
@@ -1,5 +1,16 @@
 SELECT s.NOMBRE_COMPLETO, CASE 
-    WHEN p.ID_SOCIO = ANY(SELECT DISTINCT ID_SOCIO FROM PARTICIPANTES WHERE ID_SOCIO !=2) THEN SUM(PREMIO)+(SELECT SUM(PREMIO) FROM PARTICIPANTES WHERE ID_SOCIO=2)/((SELECT COUNT (DISTINCT ID_SOCIO) FROM PARTICIPANTES)-1)
+    WHEN p.ID_SOCIO != (SELECT ID_SOCIO 
+                        FROM SOCIO
+                        WHERE FECHA_ALTA = ANY (SELECT MIN(FECHA_ALTA)
+                                                FROM SOCIO
+                                                WHERE FECHA_BAJA IS NULL))
+    THEN SUM(PREMIO)+(SELECT SUM(PREMIO)
+                      FROM PARTICIPANTES
+                      WHERE ID_SOCIO=(SELECT ID_SOCIO 
+                                      FROM SOCIO
+                                      WHERE FECHA_ALTA = ANY (SELECT MIN(FECHA_ALTA)
+                                                              FROM SOCIO
+                                                              WHERE FECHA_BAJA IS NULL)))/((SELECT COUNT (DISTINCT ID_SOCIO) FROM PARTICIPANTES)-1)
     ELSE 0
     END AS "NUEVO"
 FROM PARTICIPANTES p, SOCIO s
@@ -7,8 +18,18 @@ WHERE s.ID_SOCIO=p.ID_SOCIO
 GROUP BY p.ID_SOCIO, s.NOMBRE_COMPLETO
 HAVING
 CASE 
-    WHEN p.ID_SOCIO = ANY(SELECT DISTINCT ID_SOCIO FROM PARTICIPANTES WHERE ID_SOCIO !=2) THEN SUM(PREMIO)+(SELECT SUM(PREMIO) FROM PARTICIPANTES WHERE ID_SOCIO=2)/((SELECT COUNT (DISTINCT ID_SOCIO) FROM PARTICIPANTES)-1)
+    WHEN p.ID_SOCIO != (SELECT ID_SOCIO 
+                        FROM SOCIO
+                        WHERE FECHA_ALTA = ANY (SELECT MIN(FECHA_ALTA)
+                                                FROM SOCIO
+                                                WHERE FECHA_BAJA IS NULL))
+    THEN SUM(PREMIO)+(SELECT SUM(PREMIO)
+                      FROM PARTICIPANTES
+                      WHERE ID_SOCIO=(SELECT ID_SOCIO 
+                                      FROM SOCIO
+                                      WHERE FECHA_ALTA = ANY (SELECT MIN(FECHA_ALTA)
+                                                              FROM SOCIO
+                                                              WHERE FECHA_BAJA IS NULL)))/((SELECT COUNT (DISTINCT ID_SOCIO) FROM PARTICIPANTES)-1)
     ELSE 0
     END > 0
 ORDER BY SUM(PREMIO);
-
